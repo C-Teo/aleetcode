8de18eb6255a054d66a24b72aa001743a19a5aa0
Teo <90730116+C-Teo@users.noreply.github.com>
2023-12-16 18:19:14 -0500

    Valid Palindrome I and II

---
+++ 680_valid_palindrome_II.py
@@ -0,0 +1,48 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        res = ""
+        
+        for char in s:
+            if char.isalnum() == True:
+                res += char.lower()
+        
+        def helper(s,bool):
+            if s == "":
+                return True
+            if s[0] == s[len(s)-1]:
+                return helper(s[1:len(s)-1],bool)
+            elif bool == False:
+                return helper(s[1:len(s)],True) or helper(s[0:len(s)-1],True)
+            else:
+                return False
+
+        return helper(res,False)
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+
+        def helper(l,r,bool):
+            if l >= r:
+                return True
+            if s[l] == s[r]:
+                return helper(l+1,r-1,bool)
+            elif bool:
+                return helper(l+1,r,False) or helper(l,r-1,False)
+            else:
+                return False
+
+        return helper(l,r,True)
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+            p1=0
+            p2=len(s)-1
+            while p1<=p2:
+                if s[p1]!=s[p2]:
+                    string1=s[:p1]+s[p1+1:]
+                    string2=s[:p2]+s[p2+1:]
+                    return string1==string1[::-1] or string2==string2[::-1]
+                p1+=1
+                p2-=1
+            return True
---
+++ twoPointers/125_valid_palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+        
+        for char in s:
+            if char.isalnum() == True:
+                res += char.lower()
+
+        print(res)
+
+        if res == "":
+            return True
+
+        l = 0
+        r = len(res)-1
+
+        while l < r:
+            if res[l] != res[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
